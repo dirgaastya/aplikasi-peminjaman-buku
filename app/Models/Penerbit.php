c44ba1cc1de9a6a308924310e40ed1bae2971b5e
dirgaastya <dirga@digits.id>
2023-11-25 09:03:05 +0700

    feat(pengarang): add pengarang model & migration

---
+++ app/Models/Penerbit.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Penerbit extends Model
+{
+    use HasFactory;
+    protected $table = 'penerbits';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'address',
+        'telp',
+    ];
+}
